fd74f326d500fd2c2683a5a9e816eeff69dfe076
João Martins <joaogmartins@outlook.com>
2020-03-23 09:04:19 +0000

    revision docs

--- demo_src/src/examples/Cards.vue
+++ demo_src/src/examples/Cards.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
-    <q-selectable v-model="selected" val="Image2" selectedIcon unselectedIcon> 
-      <q-card class="my-card" :class="{'bg-primary':selected=='Image2', 'text-white':selected=='Image2'}">
+    <q-selectable v-model="selected" val="Image1" selectedIcon unselectedIcon bgSelectedColor="bg-primary"> 
+      <q-card class="my-card" :class="{'text-white':selected=='Image1'}">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section>
+        Set background-color in css to transparent, for q-selectable change the background
+      </q-card-section>
+    </q-card>
+    </q-selectable>
+
+    <q-selectable v-model="selected" val="Image2"  selectedIcon unselectedIcon bgSelectedColor="bg-red">
+      <q-card class="my-card" :class="{'text-white':selected=='Image2'}">
       <img src="https://cdn.quasar.dev/img/mountains.jpg">
 
       <q-card-section>
@@ -16,7 +31,7 @@
     </q-selectable>
 
     <q-selectable v-model="selected" val="Image3"  selectedIcon unselectedIcon>
-      <q-card class="my-card" :class="{'bg-red':selected=='Image3', 'text-white':selected=='Image3'}">
+      <q-card class="my-card" :class="{'bg-green':selected=='Image3', 'text-white':selected=='Image3'}">
       <img src="https://cdn.quasar.dev/img/mountains.jpg">
 
       <q-card-section>
@@ -25,22 +40,7 @@
       </q-card-section>
 
       <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-      </q-card-section>
-    </q-card>
-    </q-selectable>
-
-    <q-selectable v-model="selected" val="Image4"  selectedIcon unselectedIcon>
-      <q-card class="my-card" :class="{'bg-green':selected=='Image4', 'text-white':selected=='Image4'}">
-      <img src="https://cdn.quasar.dev/img/mountains.jpg">
-
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        Another way to change background color
       </q-card-section>
     </q-card>
     </q-selectable>
@@ -61,5 +61,6 @@ export default {
 .my-card{
   width: 100%;
   max-width: 250px;
+  background-color: transparent;
 }
 </style>
